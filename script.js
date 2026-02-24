4e58284df19a5217d3e0c4fb45ee743c71b1c6c8
harshitgupta0361 <harsh191205it@gmail.com>
2026-02-24 10:13:04 +0530

    primitve data types in JS

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
+// undefined
 let num;
 console.log(num);
 console.log(typeof(num));
+
+//number
 num=10;
 console.log(num);
 console.log(typeof(num));
+
+//boolean
 num =false;
 console.log(num);
 console.log(typeof(num));
+
+//string
 num ="Harshit Gupta";
 console.log(num);
 console.log(typeof(num));
+
+//null
 num =null;
 console.log(num);
 console.log(typeof(num));
 
+//function define
 var sayHello =function()
 {
     alert("hello world!");
 }
 console.log(sayHello);
 
-//SYMBOLS
 
+//SYMBOLS
 const sym1 =Symbol(4);
 console.log(sym1);
 const sym2 =Symbol(4);
