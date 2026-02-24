308701ddbf6fa6b6993414bd1a1dab36bc7186af
harshitgupta0361 <harsh191205it@gmail.com>
2026-02-24 10:31:40 +0530

    ternary operators

---
+++ script2.js
@@ -0,0 +1,14 @@
+//Largest of 3 nummbers
+let x=2; let y=3; let z=1;
+let largrest=((x>y)?((x>y)?(x):(z)):((y>z)?(y):(z)));
+document.write(largrest + "<br/>");
+
+//Loop
+var count;
+document.write("Starting Loop"+"<br/>");
+for(count=0;count<10;count++)
+{
+    document.write("Current Count: "+count);
+    document.write("<br/>");
+}
+document.write("Loop stopped!");
