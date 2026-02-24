ecf6b32a4e7660d1672a59e68039fb0a76e6de59
harshitgupta0361 <harsh191205it@gmail.com>
2026-02-24 09:46:51 +0530

    primitve data types in JS

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-console.log("Hello World");
-document.write("Hello World");
+let num;
+console.log(num);
+console.log(typeof(num));
+num=10;
+console.log(num);
+console.log(typeof(num));
+num =false;
+console.log(num);
+console.log(typeof(num));
+num ="Harshit Gupta";
+console.log(num);
+console.log(typeof(num));
+num =null;
+console.log(num);
+console.log(typeof(num));
+
+var sayHello =function()
+{
+    alert("hello world!");
+}
+console.log(sayHello);
+
+//SYMBOLS
+
+const sym1 =Symbol(4);
+console.log(sym1);
+const sym2 =Symbol(4);
+console.log(sym2);
+if(sym1==sym2)
+{
+    console.log("true");
+}
+else{
+    console.log("false");
+}
